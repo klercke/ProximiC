dc07ee6f91c9c86bb0e3e82dbaff50418929ecd0
Konnor Klercke <klercke@protonmail.com>
2020-10-19 14:47:42 -0400

    add help menu

--- src/proximic.c
+++ src/proximic.c
@@ -1,5 +1,22 @@
 #include "btsearch.h"
 
+void help(char* binName) {
+	// Print helptext for user
+	//
+	// char* binName:	Name of binary used to launch ProximiC. Assumed to be
+	// proximic if NULL.
+
+	if (binName == NULL) {
+		binName = "proximic";
+	}
+
+	printf("Usage: %s [OPTION]\n", binName);
+	printf("\n");
+	printf("-h\t\tPrint this message.\n");
+	printf("-l [TIME]\tSearch for TIME seconds, or 10 seconds if TIME is not provided.\n");
+
+}
+
 int main(int argc, char** argv) {
 	if (argc < 2){
 		printf("Usage: %s [args]\n", argv[0]);
@@ -8,7 +25,7 @@ int main(int argc, char** argv) {
 
 	char* cmd = argv[1];
 
-	if(!strcmp(cmd, "-l")) {
+	if (!strcmp(cmd, "-l")) {
 		// list nearby devices
 	
 		int time = 10; // defalts to 10 sec
@@ -22,8 +39,14 @@ int main(int argc, char** argv) {
 
 		listAllDevices(time);
 	}
+	else if (!strcmp(cmd, "-h")) {
+		// print help statement
+		help(argv[0]);
+	}
 	else {
+		// tell user input is invalid and print help statement
 		printf("Invaild command: %s\n", cmd);
+		help(argv[0]);
 	}
 
 
