16a2f767116071623f7b9732c83943b54cd37790
Konnor Klercke <klercke@protonmail.com>
2020-10-26 17:04:20 -0400

    commenting; -w option

--- src/rssiwatch.c
+++ src/rssiwatch.c
@@ -6,13 +6,17 @@
 #include "rssiwatch.h"
 
 void watchDevice(const char* mac, int time) {
+	// ensure device is connected
 	connectToMAC(mac);
 
 	int rssi;
 
+	// run forever
 	while (1) {
+		// get rssi value
 		rssi = getRSSI(mac);
 
+		// check for errors
 		if (rssi == -128) {
 			perror("Error getting RSSI");
 			exit(1);
